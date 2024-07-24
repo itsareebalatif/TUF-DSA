e7722a7874f623b864fa711f5b01d1f0d4e91c24
itsareebalatif <itsareebalatif@gmail.com>
2024-07-24 22:57:50 +0500

    most optimal solution with kadaness algo

--- Kadanes_Algorithm.py
+++ Kadanes_Algorithm.py
@@ -1,7 +1,7 @@
 import sys
 def main():
     arr1 = [-2,1,-3,4,-1,2,1,-5,4] 
-    a=brute(arr1)
+    a=maxsum(arr1)
     print(a)
 
 def brute(arr):
@@ -15,6 +15,31 @@ def brute(arr):
             maxi=max(maxi,summ) 
     return maxi           
 
+
+def better(arr):
+    n=len(arr)
+    mexi=-sys.maxsize-1
+    for i in range(n):
+        summ=0
+        for j in range(i,n):
+            summ+=arr[j]
+            mexi=mexi(mexi,summ)
+            return mexi
+    return -1        
+
+
+
+def maxsum(arr):
+    n=len(arr)
+    maxi=-sys.maxsize-1
+    summ=0
+    for i in range(n):
+        summ+=arr[i]
+        if summ>maxi:
+            maxi=summ
+        if summ<0:
+            summ=0
+    return maxi        
 
 
 
