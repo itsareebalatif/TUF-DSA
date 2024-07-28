e9acf8f1eee93ede3d3eb1655c70c733b3dc35e9
itsareebalatif <itsareebalatif@gmail.com>
2024-07-29 00:03:34 +0500

    brute force solution is written

---
+++ stock_buy_and_sell.py
@@ -0,0 +1,36 @@
+def main():
+    prices = [7,1,5,3,6,4]
+    a=stock_price(prices)
+    print(a)
+
+
+
+def stock_price(arr):
+    minprice=0
+    maxpro=0
+    n=len(arr)
+    for i in range(n):
+        for j in range(i+1,n):
+            if arr[j]>arr[i]:
+                maxpro=max(maxpro,arr[j]- arr[i])
+    return maxpro            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
