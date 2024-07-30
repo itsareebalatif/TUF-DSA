f625fba8d2e95ff2c2b98d4d1d1e2808cc47f3f4
itsareebalatif <itsareebalatif@gmail.com>
2024-07-30 10:06:40 +0500

    optimal

--- rearrange_array_sign.py
+++ rearrange_array_sign.py
@@ -1,9 +1,10 @@
 def main():
     arr1=[3,1,-2,-5,2,-4]
+    #rearrange_brute(arr1)
     rearrange_optimal(arr1)
 
 
-def rearrange_optimal(arr) :
+def rearrange_brute(arr) :
     n=len(arr)
     
     positive_arr=[]
@@ -23,6 +24,24 @@ def rearrange_optimal(arr) :
 
 
 
+
+
+
+def rearrange_optimal(arr):
+    n=len(arr)
+    ans=[]
+    positive_index=0
+    negative_index=1
+    for i in range(n):
+        if arr[i]<0:
+            ans[negative_index]=arr[i]
+            negative_index+=2
+            print(ans)
+        else:
+            ans[positive_index]=arr[i]
+            positive_index+=2
+            print(ans)    
+    print("new:",ans)            
 
 
 
