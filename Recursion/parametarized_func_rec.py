fc71b979eab1667ac78cf56bdb5597ea30c8faf9
itsareebalatif <itsareebalatif@gmail.com>
2024-08-01 02:34:37 +0500

    parametrized way of recursion

--- Recursion/parametarized_func_rec.py
+++ Recursion/parametarized_func_rec.py
@@ -1,12 +1,18 @@
 def main():
-    a=summofno(5)
-    print(a)
+    SUMMOFNO(3,0)
+    
 
 def summofno(N): 
     if N==0:
         return 0
     return N+summofno(N-1)
 
+def SUMMOFNO(n,sum):
+    if n==0:
+        print(sum)
+        return 0
+    return SUMMOFNO(n-1,sum+n)
+    
 
 
 
