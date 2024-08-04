85a4b5d2c8285bd006c10d790e9a28609ba41e79
itsareebalatif <itsareebalatif@gmail.com>
2024-08-05 02:43:16 +0500

    brute force of Range Sum of Sorted Subarray Sums

---
+++ Arrays/rnage_sum_ofsorrted_subarraysum.py
@@ -0,0 +1,26 @@
+def main():
+
+    arr = [1,2,3,4]
+    # 1, 3, 6, 10, 2, 5, 9, 3, 7, 4.
+    a=bruteforce(arr,1,5)
+    print(a)
+
+def bruteforce(nums,left,right):
+    res=[]
+    for i in range(len(nums)):
+        
+        for j in range(i,len(nums)):
+            summ=0
+            summ+=nums[j]
+            res.append(summ)
+
+    res.sort() 
+    sum=0
+    
+    for i in range(left-1,right) :
+        sum+=res[i]
+
+    return sum
+
+if __name__=="__main__":
+    main()
